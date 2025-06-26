e4e621d2c3175b36657a407dfef41124335576ff
ILHAM051004 <ilham23ti@mahasiswa.pcr.ac.id>
2025-06-26 23:10:13 +0700

    Fix: install axios

--- src/services/AppService.js
+++ src/services/AppService.js
@@ -1,4 +1,4 @@
-import axios from 'axios'
+import axios from "axios";
 
 const API_URL = "https://hyisnaqhamncugeppsnx.supabase.co/rest/v1/prediksi"
 const API_KEY = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Imh5aXNuYXFoYW1uY3VnZXBwc254Iiwicm9sZSI6ImFub24iLCJpYXQiOjE3NTA1MDk2MDUsImV4cCI6MjA2NjA4NTYwNX0.IqI78WfiH2b4rWA9tFdk4rF-KKOipS9XWlVQAJkZ2BU"
